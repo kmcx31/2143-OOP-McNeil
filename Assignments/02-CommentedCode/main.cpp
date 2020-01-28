624e12ac555ffc299082b5b4c5602a8de328199c
kmcx31 <shaqmantoo121@yahoo.com>
2020-01-28 00:07:39 -0600

    complete assignment 2

--- Assignments/02-CommentedCode/main.cpp
+++ Assignments/02-CommentedCode/main.cpp
@@ -10,9 +10,8 @@
 //       Implements a doubley-linked list, automatically sorts and places elements into list, and prints output.
 //
 // Usage:
-//       how to use the program if necessary
 //
-// Files:            (list of all source files used in this program)
+// Files:   main.cpp    :   driver program
 ///////////////////////////////////////////////////////////////////////////////
 
 #include <iostream>
@@ -39,13 +38,40 @@ struct Node
     }
 };
 
-//class implementation of doubley linked list
+/**
+ * List
+ * 
+ * Description:
+ *      This class implements a list of nodes that hold single
+ *      value integers with the capability of sorting the values as they are added
+ *      and printing them out.
+ * 
+ * Public Methods:
+ *      - void      Push(int val)
+ *      - void      Insert(int val)
+ *      - void      PrintTail()
+ *      - string    Print()
+ *      - int       Pop()
+ *      - List      operator+(const List &Rhs)
+ *      - int       operator[](int index)
+ *      - friend    ostream &operator<<(ostream &os, List L)
+ * 
+ * Private Methods:
+ * 
+ * Usage:
+ *      List L;         //initialize list
+ * 
+ *      L.Push(int);    //push value into list
+ *      L.Pop();        //return value from list
+ *      L.Print():      //print list
+ *      
+ */
 class List
 {
   private:
-    Node *Head;
-    Node *Tail;
-    int Size;
+    Node *Head;     //pointer to track from of list
+    Node *Tail;     //pointer to track end of list
+    int Size;       //size counter to keep track of total size of list
 
   public:
     //default constructor
@@ -54,6 +80,18 @@ class List
         Head = Tail = NULL;
         Size = 0;
     }
+
+    /**
+     * Public : Push
+     * 
+     * Description:
+     *      Push value onto list
+     * 
+     * Params:
+     *      int     :   numeral value
+     * 
+     * Returns:
+     */
 
     void Push(int val)
     {
@@ -72,6 +110,18 @@ class List
         Size++;
     }
 
+    /**
+     * Public : Insert
+     * 
+     * Description:
+     *      Insert value into list.
+     * 
+     * Params:
+     *      int     :   numeral value
+     * 
+     * Returns:
+     */
+
     void Insert(int val)
     {
         // allocate new memory and init node
@@ -88,10 +138,32 @@ class List
         Size++;
     }
 
+    /**
+     * Public : PrintTail
+     * 
+     * Description:
+     *      Prints the last value of the list.
+     * 
+     * Params:
+     * 
+     * Returns:
+     */
+
     void PrintTail()
     {
         cout << Tail->x << endl;
     }
+
+    /**
+     * Public : Print
+     * 
+     * Description:
+     *      Prints the List values
+     * 
+     * Params:
+     * 
+     * Returns:
+     */
 
     string Print()
     {
@@ -107,12 +179,36 @@ class List
         return list;
     }
 
-    // not implemented 
+    /**
+     * Public : Pop
+     * 
+     * Description:
+     *      Remove and return a value from the end of the List.
+     * 
+     * Params:
+     * 
+     * Returns:
+     *      int value from list.
+     */
+
     int Pop()
     {
         Size--;
         return 0; //
     }
+
+    /**
+     * Public : operator+
+     * 
+     * Description:
+     *      Copies a previously made List to a new List.
+     * 
+     * Params:
+     *      cont List&    :   An already made List
+     * 
+     * Returns:
+     *      A new List with values from old List.
+     */
 
     List operator+(const List &Rhs)
     {
@@ -174,15 +270,15 @@ class List
 
 int main(int argc, char **argv)
 {
-    List L1;
-    List L2;
+    List L1;    //initialize l1
+    List L2;    //initialize l2
 
-    for (int i = 0; i < 25; i++)
+    for (int i = 0; i < 25; i++)    //for loop that pushes 0-24 onto L1
     {
         L1.Push(i);
     }
 
-    for (int i = 50; i < 100; i++)
+    for (int i = 50; i < 100; i++)  //for loop that pushes 50-99 onto L2
     {
         L2.Push(i);
     }
@@ -191,7 +287,7 @@ int main(int argc, char **argv)
     L1.PrintTail();
     L2.PrintTail();
 
-    List L3 = L1 + L2;
+    List L3 = L1 + L2;      //adds values of L1 and L2 togeth to create L3
     cout << L3 << endl;
 
     cout << L3[5] << endl;
